24189379aa1a01530cd17e66525d45b8895be0a1
esmameral <esma.meral@gmail.com>
2021-01-13 19:09:09 +0300

    sample project about integrating reactjs with Boootstrap

---
+++ CS391-WebProgramming/Week14/sample-with-reactstrap/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Table from './components/Table';
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <Table />
+      </div>
+    );
+  }
+}
+
+export default App;
