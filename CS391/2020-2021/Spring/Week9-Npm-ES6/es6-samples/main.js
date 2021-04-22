92ec9243f2b9d45b20f1f11a5ba719f5ea39b1d1
esmameral <esma.meral@gmail.com>
2021-04-22 09:13:37 +0300

    Folder organization

---
+++ CS391/2020-2021/Spring/Week9-Npm-ES6/es6-samples/main.js
@@ -0,0 +1,20 @@
+import {
+    variables,
+    generatorConsume,
+    templates,
+    arrowFunctionExpression,
+    arrowFunctionMapFilter,
+    destructuringAssignments,
+    objectLiterals
+    
+} from './operations'
+
+variables();
+generatorConsume();
+templates();
+arrowFunctionExpression();
+arrowFunctionMapFilter();
+destructuringAssignments();
+objectLiterals();
+
+
